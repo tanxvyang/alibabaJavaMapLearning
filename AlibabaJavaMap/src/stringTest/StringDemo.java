99df972bf96a09a1cd5bd99af5a511d2fc93f898
‘tanxvyang’ <tanxvyang@163.com>
2022-01-13 15:23:20 +0800

    数据表与简单Java类的映射关系

---
+++ AlibabaJavaMap/src/stringTest/StringDemo.java
@@ -0,0 +1,26 @@
+package stringTest;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author tanxy
+ * @version 1.0.0
+ * @ClassName StringDemo.java
+ * @Description TODO
+ * @createTime 2021年12月28日 15:21:00
+ */
+public class StringDemo {
+
+
+    public static void main(String[] args) {
+//       实现字符串比较
+//        String strA= new String("mlda");
+//        String strB= "mlda";
+//        System.out.println(strA==strB);
+//
+
+
+
+
+    }
+}
